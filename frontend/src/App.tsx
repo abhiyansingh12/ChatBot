4c9e2b4af4721c07ea478c223687571ba2990f12
AbSingh <abhiyans0@gmail.com>
2023-12-15 15:12:35 -0600

    Added new files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,31 @@
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Homes";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Chat from "./pages/Chat";
+import NotFound from "./pages/Notfound";
+import { useAuth } from "./components/context/AuthContet";
+import Footer from "./components/footer/Footer";
+
+
+function App() {
+  const auth = useAuth();
+
+  return (
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {auth?.isLoggedIn && auth.user && (
+          <Route path="/chat" element={<Chat />} />
+        )}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/typer/TypingAnim.tsx
@@ -0,0 +1,25 @@
+import { TypeAnimation } from "react-type-animation";
+
+const TypingAnim = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed once, initially
+        "Ask you queries",
+        1000,
+        "We will come up with best possible solutions",
+        1500,
+      ]}
+      speed={50}
+      style={{
+        fontSize: "60px",
+        color: "white",
+        display: "inline-block",
+        textShadow: "1px 1px 20px #000",
+      }}
+      repeat={Infinity}
+    />
+  );
+};
+
+export default TypingAnim;
